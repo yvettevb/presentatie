3d530f0cf5b9f49dd269a992d19b452fcc70c514
yvettevb <126894351+yvettevb@users.noreply.github.com>
2023-04-08 19:37:42 +0200

    Update Final.py

--- Final.py
+++ Final.py
@@ -141,7 +141,7 @@ df_mag=df_mag.rename(columns={"index": "Magnitude", "magnitude": "Count"})
 
 fig2 = px.bar(df_mag, x="Magnitude", y='Count')
 fig2.update_layout(xaxis = dict(dtick = 0.1))
-st.bar_chart(fig2)
+st.plotly_chart(fig2, theme = 'streamlit')
 
 # In[12]:
 
