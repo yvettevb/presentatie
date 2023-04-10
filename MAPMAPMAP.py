d70784610e1cf9108e67e8572b8e82ac91a8a269
yvettevb <126894351+yvettevb@users.noreply.github.com>
2023-04-10 20:44:23 +0200

    Update MAPMAPMAP.py

--- MAPMAPMAP.py
+++ MAPMAPMAP.py
@@ -10,11 +10,57 @@
 #!pip install streamlit-folium
 #!pip install folium
 
+import requests
+
 import pandas as pd
-import plotly.express as px
-import folium 
+
+import folium
+
 import streamlit as st
-#from streamlit_folium import st_folium
+
+from streamlit_folium import st_folium
+
+from PIL import Image
+
+import calendar
+
+import time
+
+from datetime import datetime as dt
+
+from datetime import date
+
+import plotly.express as px
+
+import plotly.figure_factory as ff
+
+import seaborn as sns
+
+from statsmodels.formula.api import ols
+
+from matplotlib import style
+
+import matplotlib.pyplot as plt
+
+from sklearn import metrics
+
+from sklearn import model_selection
+
+from sklearn.linear_model import LogisticRegression
+
+from sklearn.model_selection import train_test_split
+
+from sklearn.preprocessing import StandardScaler
+
+from sklearn.linear_model import LinearRegression
+
+from sklearn import metrics
+
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+
+import numpy as np
+
+import urllib.request
 
 
 # In[50]:
